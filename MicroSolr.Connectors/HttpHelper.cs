1b5c3feae2883b9706a6431110997c453ae1fa7c
Imran Saeed <imran.saeed.m.is@gmail.com>
2012-09-05 18:13:56 +0100

    First stab at XML

--- MicroSolr.Connectors/HttpHelper.cs
+++ MicroSolr.Connectors/HttpHelper.cs
@@ -10,11 +10,6 @@ namespace MicroSolr.Connectors
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Net;
     using System.IO;
 
