a6226629e4183120a98cc37155c0f8608c32b109
Imran Saeed <imran.saeed.m.is@gmail.com>
2012-09-05 14:47:08 +0100

    NuGet Support

--- MicroSolr.Connectors/Properties/AssemblyInfo.cs
+++ MicroSolr.Connectors/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MicroSolr.Connectors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple fast/parallel connector for SOLR connectivity")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EF")]
 [assembly: AssemblyProduct("MicroSolr.Connectors")]
