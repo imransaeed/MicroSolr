5a9898a44edd2ce125fc32f003ce095f7aa73e68
Imran Saeed <imran.saeed.m.is@gmail.com>
2012-09-11 21:20:49 +0100

    Server changes

---
+++ MicroSolr.Core/Clients/HttpClient.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HttpClient.cs" company="EF">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MicroSolr.Core.Clients
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class HttpClient : IClient
+    {
+        public HttpClient(Uri baseUri, params ICore[] cores)
+        {
+            BaseUri = baseUri;
+            Cores = new List<ICore>(cores);
+            DefaultCore = Cores.First();
+        }
+
+        public Uri BaseUri
+        {
+            get;
+            private set;
+        }
+
+        public IList<ICore> Cores
+        {
+            get;
+            private set;
+        }
+
+        public ICore DefaultCore
+        {
+            get;
+            private set;
+        }
+    }
+}
