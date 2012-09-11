48a041ac0410e1a20c401967074cc01f4bc5e4f4
Imran Saeed <imran.saeed.m.is@gmail.com>
2012-09-11 21:20:49 +0100

    Server changes

---
+++ MicroSolr.Core/IClient.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IClient.cs" company="Imran Saeed">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MicroSolr.Core
+{
+    using System;
+    using System.Net;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface IClient
+    {
+        Uri BaseUri { get; }
+
+        IList<ICore> Cores { get; }
+
+        ICore DefaultCore { get; }
+    }
+}
