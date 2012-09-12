ad9bfa27b4933a1bd5a13133203a7382ef6a94b6
Imran Saeed <imran.saeed.m.is@gmail.com>
2012-09-12 15:34:31 +0100

    Publish ready

--- MicroSolr.Core/Properties/AssemblyInfo.cs
+++ MicroSolr.Core/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MicroSolr.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core MicroSolr library. A simple and fast .Net client for Solr.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EF")]
+[assembly: AssemblyCompany("Imran Saeed")]
 [assembly: AssemblyProduct("MicroSolr.Core")]
-[assembly: AssemblyCopyright("Copyright © EF 2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Imran Saeed 2012")]
+[assembly: AssemblyTrademark("MicroSolr")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
